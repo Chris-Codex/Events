795aa459731670907dc29f9fc94eb68c466d7c35
Christian Iheacho <christianiheacho@MACBOOKAIR-FFB6.station>
2025-07-12 08:26:32 +0100

    first commit

---
+++ creative-event/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='flex justify-between items-center pl-15 pr-15 w-full h-16 shadow-md'>
+        <h1 className='text-[#7B61FF] text-2xl font-bold'>CreativeEvents</h1>
+        <nav>
+            <ul>Navs</ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
